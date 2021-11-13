3f62665c89405cb1eda272deafdca16617b32949
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-11-13 15:37:06 -0800

    Fixing zinit (zdharma org was removed) Moving everything to zdharma-continuum

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,8 +1,8 @@
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
     command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
         print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
@@ -14,18 +14,18 @@ autoload -Uz _zinit
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
-    zinit-zsh/z-a-rust \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-bin-gem-node
+    zdharma-continuum/zinit-annex-rust \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-bin-gem-node
 ### End of Zinit's installer chunk
 
 zinit wait lucid light-mode for \
     atinit'ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay' \
-        zdharma/fast-syntax-highlighting \
+        zdharma-continuum/fast-syntax-highlighting \
         OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh \
-    atload'_zsh_autosuggest_start' zsh-users/zsh-autosuggestions \
-    blockf atpull'zinit creinstall -q .' zsh-users/zsh-completions \
+    blockf zsh-users/zsh-completions \
+    atload'!_zsh_autosuggest_start' zsh-users/zsh-autosuggestions \
     svn pick'completion.zsh' multisrc'git.zsh history.zsh' OMZ::lib
 
 zinit wait lucid light-mode for \
