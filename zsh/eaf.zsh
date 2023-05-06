be18c822d11f2644436ffae6367857a88fc76f20
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-05-05 20:02:08 -0700

    fzf improvements

--- zsh/eaf.zsh
+++ zsh/eaf.zsh
@@ -3,7 +3,9 @@ export EDITOR_NOWAIT='code'
 export BAT_STYLE='full'
 export BAT_PAGER='less -rRXF'
 export GPG_TTY=$(tty)
-export FZF_CTRL_T_OPTS="--bind='enter:execute(code {})+abort' --preview 'bat --color \"always\" {}'"
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window up:3:hidden:wrap --bind 'ctrl-/:toggle-preview' --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' --color header:italic --header 'Press enter to execute, ^Y to copy, ^/ to preview'"
+export FZF_CTRL_T_OPTS="--preview 'bat --color always {}' --bind='enter:execute(code {})+abort' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
 export COPYFILE_DISABLE=1 # so that tar works properly on mac
 
 export GOPATH="$HOME/go"
